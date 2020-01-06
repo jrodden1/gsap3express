d623db399b1c1d1650a70b214e219837821d68b7
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-06 11:58:16 -0700

    Adding in instructional notes

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ gsap.fromTo()
 
 // A Tween can change a single propery of a single object over time
 gsap.to(".star", {x:750, duration:3})
+// the .to method takes the element we specify (in this case anything with .star class) and animates its properties to the values we specify (in this case, animating the x property over a duration of 3 seconds).
+// the ".star" part of the command above is the Target
+// next is the vars object ({x:750, duration:3}) that contains the properties we are animating and a special series of properties that tell us how the tween is going to run (duration) .  Also note that the "x:" property is the shorthand for css translateX(750px) property

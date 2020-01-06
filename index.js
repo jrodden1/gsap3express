2cc7f57ac277e48704c4c83ed96dc2b5fc5bef53
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-06 11:51:15 -0700

    saved star.svg original file in new media folder; added basic animation for star graphic

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//very basic animation moving the h2 element on the x axis 400 pixels
 gsap.to("h2", {x:400});
+
+/* 
+Gsap has 3 main methods for creating Tweens and optionally adding them to timelines:
+gsap.to()
+gsap.from()
+gsap.fromTo()
+*/
+
+// A Tween can change a single propery of a single object over time
+gsap.to(".star", {x:750, duration:3})
